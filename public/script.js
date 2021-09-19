f3acc7089e21a9274f8cd160e7332426055b8d19
himanshu76200 <himanshu76200@gmail.com>
2021-09-19 19:53:44 +0530

    video chat app

---
+++ public/script.js
@@ -0,0 +1,58 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid")
+const peer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+const peers = {};
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream);
+
+    peer.on('call', (call) => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+
+socket.on('user-disconnected', userId => {
+    if (peers[userId]) {
+        peer[userId].close()
+    }
+})
+
+peer.on('open', id => {
+    socket.emit("join-room", ROOM_ID, id)
+})
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+    videoGrid.append(video)
+}
+
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+    peer[userId] = call;
+}
